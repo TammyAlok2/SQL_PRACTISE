78e4c8cd3aa2863ac90dcd639666c31a392446b3
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2026-02-06 19:12:59 +0530

    feat - Done with filtering data learnt about operators , AND , OR , NOT , LIKE , BETWEEN and many others

--- 04_FILTERING_DATA.sql
+++ 04_FILTERING_DATA.sql
@@ -55,3 +55,62 @@ WHERE score <= 500
 =============================================================================== */
 
 /* Combining conditions using AND, OR, and NOT */
+
+-- Retrieve all customers who are from the USA and have a score greater than 500.
+SELECT * FROM customers
+WHERE country = 'USA' AND score > 500
+
+-- Retrieve all customers who are either from the USA or have a score greater than 500.
+SELECT * FROM customers
+WHERE country = 'USA' OR score > 500
+
+-- Retrieve all customers with a score not less than 500.
+SELECT * FROM customers
+WHERE score < 500
+
+/* ============================================================================== 
+   RANGE FILTERING - BETWEEN
+=============================================================================== */
+
+-- Retrieve all customers whose score falls in the range between 100 and 500.
+SELECT * FROM customers
+WHERE SCORE BETWEEN 50 AND 100
+
+-- Alternative method (Equivalent to BETWEEN)
+SELECT * FROM customers
+WHERE SCORE >= 50 AND SCORE <=100
+
+
+/* ============================================================================== 
+   SET FILTERING - IN
+=============================================================================== */
+
+-- Retrieve all customers from either Germany or the USA.
+SELECT * FROM customers
+WHERE country in ("USA","Russia","China")
+
+SELECT * FROM customers
+WHERE country = 'USA'
+OR country = 'Russia'
+OR country ='China'
+
+
+/* ============================================================================== 
+   PATTERN MATCHING - LIKE
+=============================================================================== */
+
+-- Find all customers whose first name starts with 'M'.
+SELECT * FROM customers
+WHERE first_name LIKE 'v%'
+
+-- Find all customers whose first name ends with 'n'.
+SELECT * FROM customers
+WHERE first_name LIKE '%h'
+
+-- Find all customers whose first name contains 'r'.
+SELECT * FROM customers
+WHERE first_name LIKE '%a%'
+
+-- Find all customers whose first name has 'r' in the third position.
+SELECT * FROM customers
+WHERE first_name LIKE '___a%'
